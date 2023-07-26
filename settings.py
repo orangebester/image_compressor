865c59245374d0b84baed4356f84fed45ca4de61
Vitalii Kosyi <vkosyi@n-ix.com>
2023-07-26 10:13:00 +0300

    Readme fixed

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 # write path of source directory with images
-src_dir_path = r"C:\Users\vkosyi\Documents\Study zone\Dzyga\Image_Compressor\input"
+src_dir_path = r"input"
 # write path of directory where you want to store compressed images
-dst_dir_path = r"C:\Users\vkosyi\Documents\Study zone\Dzyga\Image_Compressor\output"
+dst_dir_path = r"output"
 # specify a file size limit for image to be compressed
 optimal_size = 1048576
 # specify desirable file size format, which compressed image should have
